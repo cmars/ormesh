ed12bfbcb04086ba8545c57cb38463cfb9f13590
Casey Marshall <git@cmars.tech>
2017-11-15 21:43:03 -0600

    Add snapcraft.

--- config/config_linux.go
+++ config/config_linux.go
@@ -2,8 +2,17 @@
 
 package config
 
+import (
+	"os"
+	"path/filepath"
+)
+
 func (c *Config) platformDefaults() {
 	if c.Node.Agent.TorBinaryPath == "" {
-		c.Node.Agent.TorBinaryPath = "/usr/bin/tor"
+		if snapDir := os.Getenv("SNAP"); snapDir != "" {
+			c.Node.Agent.TorBinaryPath = filepath.Join(snapDir, "usr", "bin", "tor")
+		} else {
+			c.Node.Agent.TorBinaryPath = "/usr/bin/tor"
+		}
 	}
 }
