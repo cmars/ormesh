5956da9d838349f27facb35967a2e11fa99ec454
Casey Marshall <me@cmars.tech>
2017-11-22 14:31:47 -0600

    Add install script.

---
+++ install.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+echo "deb http://deb.torproject.org/torproject.org xenial main" | sudo tee /etc/apt/sources.list.d/tor.list
+echo "deb-src http://deb.torproject.org/torproject.org xenial main" | sudo tee -a /etc/apt/sources.list.d/tor.list
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
+sudo apt update
+sudo apt upgrade -yy
+sudo apt install -y tor deb.torproject.org-keyring
+
+tmpdir=$(mktemp -d)
+trap "rm -rf ${tmpdir}"
+
+cd ${tmpdir}
+wget -O ormesh.tar.gz https://github.com/cmars/ormesh/releases/download/v0.1.5/ormesh_0.1.5_linux_amd64.tar.gz
+tar xf ormesh.tar.gz
+sudo cp ormesh /usr/bin/ormesh
+sudo /usr/bin/ormesh agent privbind
+/usr/bin/ormesh agent systemd | sudo tee /etc/systemd/system/ormesh.service
+
