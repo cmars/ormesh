9f07b1ad32b03a29cbfdef3fb87b3b1cb2701e1d
Casey Marshall <me@cmars.tech>
2017-11-24 22:49:57 -0600

    Fix sed.

--- release.bash
+++ release.bash
@@ -10,7 +10,7 @@ fi
 
 set -u
 
-sed -i 's/^version: .*/version: \''${VERSION}'\'' snap/snapcraft.yaml
+sed -i "s/^version: .*/version: '${VERSION}'/" snap/snapcraft.yaml
 git reset
 git add snap/snapcraft.yaml
 git commit -m "Release snapcraft ${VERSION}"
