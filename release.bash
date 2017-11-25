c54a432b5c90e3fa7923316b7515be6a7167186c
Casey Marshall <me@cmars.tech>
2017-11-24 22:51:16 -0600

    Skip committing snapcraft if unchanged.

--- release.bash
+++ release.bash
@@ -13,7 +13,7 @@ set -u
 sed -i "s/^version: .*/version: '${VERSION}'/" snap/snapcraft.yaml
 git reset
 git add snap/snapcraft.yaml
-git commit -m "Release snapcraft ${VERSION}"
+git commit -m "Release snapcraft ${VERSION}" || true
 
 git tag -s -a "v${VERSION}" -m "Release ${VERSION}"
 git push origin "v${VERSION}"
