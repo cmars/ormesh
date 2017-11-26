f1e05a7c1c808103b27cc2068749529c263e3f8c
Casey Marshall <me@cmars.tech>
2017-11-26 13:04:44 -0600

    Fix variable scoping.

--- config/config_windows.go
+++ config/config_windows.go
@@ -24,11 +24,11 @@ import (
 )
 
 func (c *Config) platformDefaults() {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatalf("failed to locate home directory: %v", err)
+	}
 	if c.Node.Agent.TorBinaryPath == "" {
-		home, err := homedir.Dir()
-		if err != nil {
-			log.Fatalf("failed to locate home directory: %v", err)
-		}
 		c.Node.Agent.TorBinaryPath = filepath.Join(home, "Desktop",
 			"Tor Browser", "Browser", "TorBrowser", "Tor", "tor.exe")
 	}
